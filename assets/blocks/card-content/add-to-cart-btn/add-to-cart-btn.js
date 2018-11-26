eed12873cf04f27a1fc6bd49a5d3e90be24a64e2
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2018-11-26 13:53:45 +0700

    New Component added, parent component class fixed
    • Added a condition to parent Component class to make 'options'
    parameter optional, for some elements do not require any extra data
    to be inserted into layout;
    • Added a button to add items to cart

---
+++ assets/blocks/card-content/add-to-cart-btn/add-to-cart-btn.js
@@ -0,0 +1,13 @@
+import { Component } from "../../component";
+import layout from 'layout.pug';
+import _ from './styles.scss';
+
+export class AddToCartBtn extends Component{
+    constructor(data) {
+        super(data);
+    }
+
+    render() {
+        $(this.el).html(layout(this.options));
+    }
+}
