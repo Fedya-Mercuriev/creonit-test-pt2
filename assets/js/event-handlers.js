2588680f0ce0b61aa5f64ca7119b27008894b423
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2018-11-28 22:45:28 +0700

    Added squeeze effect to card body
    Previously when hovering cursor over some parts of card animation didn't
    work. When delegated it to script, animation works wherever you hover
    cursor over

--- assets/js/event-handlers.js
+++ assets/js/event-handlers.js
@@ -1,12 +1,15 @@
-let eventHandlers = {
+export class EventHandlers {
+  cosntructor() {
+
+  }
     // Эта функция подсвечивает выбранные элементы
-    highlightElems(elemsArr, needsHighlight) {
+    highlightElems(elemsArr, needsHighlight, modifier) {
       if (needsHighlight) {
         $.each(elemsArr, (_, element) => {
           // Получим класс текущего элемента
           let currElemClass  = $(element).attr('class');
           // Подсветим элемент
-          $(element).addClass(`${currElemClass}--highlighted`);
+          $(element).addClass(`${currElemClass}${modifier}`);
         })
       } else {
         $.each(elemsArr, (_, element) => {
@@ -16,14 +19,17 @@ let eventHandlers = {
           // Подсветим элемент
           $(element).removeClass(`${classToRemove}`);
         })
-        // for (let i = 0; i < elemsArr[i]; i++) {
-        //   // Получим класс текущего элемента
-        //   let currElemClass = $(elemsArr[i]).attr('class');
-        //   // Подсветим элемент
-        //   $(elemsArr[i]).removeClass(`${currElemClass}--highlighted`);
-        // }
       }
     }
-  };
 
-export {eventHandlers};
+    addSqueezeEffect(parent) {
+      let $cardBody = $(parent).find('div.card-body');
+      // Если карточка была нажата, при ее наведении ничего не происходит
+      if ($cardBody.hasClass('card-body--card-expanded')) {
+        return;
+      } else {
+        $cardBody.toggleClass('card-body--hovered');
+      }
+     }
+
+  }
