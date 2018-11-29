5853cd12259a0cd548252a967be3c85a0d56330e
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2018-11-29 18:43:40 +0700

    Fixed card click event handler

--- assets/views/products/products.js
+++ assets/views/products/products.js
@@ -1,20 +1,40 @@
+import { View } from "../view";
 import { CardView } from "../card/card";
 import _ from './styles.scss';
 
-export class ProductsView {
+export class ProductsView extends View{
 
     constructor(el) {
-        this.el = el;
+        super(el);
+        this.cards = [];
+
+        // Обрабатываем клики по карточкам
+        $(this.el).on('click', 'div.card', (event) => {
+            this.cards.forEach((card) => {
+                let target = event.currentTarget;
+                // Таргетом явлется сама карточка или вложенный элемент?
+                if (target == card.el) {
+                    (card.isOpen) ? card.wrap() : card.expand();
+                } else {
+                    if (!card.isOpen) {
+                        return
+                    } else {
+                        card.wrap();
+                    }
+                }
+            })
+        })
     }
 
     render(options) {
         for (let i = 0; i < options.length; i++) {
             let opts = options[i],
-                $newElement = $(document.createElement('div'));
-            $newElement.addClass('card');
-            $(this.el).append($newElement);
-            const cardView = new CardView('.card:last');
+                newElement = document.createElement('div');
+            $(newElement).addClass('card');
+            $(this.el).append(newElement);
+            let cardView = new CardView(newElement);
             $(this.el).append(cardView.render(opts));
+            this.cards.push(cardView);
         }
     }
 
