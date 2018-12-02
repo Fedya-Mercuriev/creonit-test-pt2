a27a63725fc56c578a4d4b7d9ad5ac408f6186ac
Fedya-Mercuriev <gleb.gorkoltsev@yandex.ru>
2018-12-02 16:17:06 +0700

    Fixed click-event handlers for cards

--- assets/views/products/products.js
+++ assets/views/products/products.js
@@ -2,7 +2,7 @@ import { View } from "../view";
 import { CardView } from "../card/card";
 import _ from './styles.scss';
 
-export class ProductsView extends View{
+export class ProductsView extends View {
 
     constructor(el) {
         super(el);
@@ -10,34 +10,69 @@ export class ProductsView extends View{
 
         // Обрабатываем клики по карточкам
         $(this.el).on('click', 'div.card', (event) => {
-            this.cards.forEach((card) => {
-                let target = event.currentTarget;
-                // Таргетом явлется сама карточка или вложенный элемент?
-                if (target == card.el) {
-                    if (card.isOpen) {
-                        card.wrap();
+            // let addToCartBtn = $(event.currentTarget).find('.add-to-cart-btn');
+            if ($(event.target).is('a')) {
+                return;
+            } else {
+                this.cards.forEach((card) => {
+                    let target = event.currentTarget;
+                    // Таргетом явлется сама карточка или вложенный элемент?
+                    if (target === card.el) {
+                        if (card.isOpen) {
+                            card.wrap();
+                        } else {
+                            card.expand();
+                        }
                     } else {
-                        card.expand();
+                        if (card.isOpen) {
+                            card.wrap();
+                        }
                     }
-                } else {
-                    if (!card.isOpen) {
-                        return
-                    } else {
-                        card.wrap();
-                    }
-                }
-            })
-            event.stopPropagation();
+                    event.stopPropagation();
+                });
+            }
         });
 
-        // $(document.body).click(() => {
+        // ТОЧНАЯ КОПИЯ
+        // $(this.el).on('click', 'div.card', (event) => {
         //     this.cards.forEach((card) => {
-        //         if (card.isOpen) {
-        //             card.wrap();
+        //         let target = event.currentTarget;
+        //         // Таргетом явлется сама карточка или вложенный элемент?
+        //         if (target === card.el) {
+        //             if (card.isOpen) {
+        //                 card.wrap();
+        //             } else {
+        //                 card.expand();
+        //             }
         //         } else {
-        //             return;
+        //             if (card.isOpen) {
+        //                 card.wrap();
+        //             }
         //         }
-        //     })
+        //     });
+        //     event.stopPropagation();
+        // });
+
+        // $(this.el).on('click', (event) => {
+        //     // Посмотрим на что нажал пользователь исходя из currentTarget
+        //     let eventTargetParentClass = $(event.currentTarget).attr('class').split(' ')[0],
+        //         productWrapperClass = $(this.el).attr('class').split(' ')[0];
+        //     // Если кликнули на кнопку добавки в корзину
+        //     if (eventTargetParentClass === 'card') {
+        //         return;
+        //     }
+        //     // Если кликнули по самой карточке или ее детям, кроме кнопки добавки в корзину
+        //     if (eventTargetParentClass === productWrapperClass) {
+        //         let parents = $(event.target).parentsUntil('.card');
+        //         console.log(parents);
+        //         $.each(this.cards, (_, card) => {
+        //             if (card.isOpen) {
+        //                 card.wrap();
+        //             } else {
+        //                 card.expand();
+        //             }
+        //         });
+        //     }
         // })
     }
 
